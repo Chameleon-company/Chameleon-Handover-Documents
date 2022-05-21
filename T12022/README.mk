5a308386da7285221cfbf4953edb8d2d3722657e
molliefernandez <102345163+molliefernandez@users.noreply.github.com>
2022-05-21 10:01:03 +1000

    Create README.mk

---
+++ T12022/README.mk
@@ -0,0 +1,10 @@
+Chameleon Company Trimester 1 2022
+
+
+This folder contains subfolders from each of the projects this trimester. These folders contain handover documents and any other relevent material from that projects team to help the next trimesters students continue on the project.
+
+
+
+- City Of Melbourne Open Data Project
+- Chameleon Website
+- EV Adpotion Tools
